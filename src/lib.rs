f3966729e300560c6eccb844f046f09ed45ce4e1
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2023-03-07 01:43:28 +0100

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -15,13 +15,6 @@ fn kmer(_py: Python, m: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
-// const CHARACTERS: Vec<&str> = vec!["a", "c", "g", "t"];
-// const KMER_LEN: u32 = 9;
-// static KMER_INDEX_MAP: HashMap<&str, i32> = (2..KMER_LEN).fold(
-//     ("a", "c", "g", "t").iter().map(|c| ("a", "c", "g", "t").iter().map(move |&d| d.to_owned() + *c)).flatten().collect(),
-//     |acc,_| acc.into_iter().map(|c| ("a", "c", "g", "t").iter().map(move |&d| d.to_owned() + &*c)).flatten().collect())
-//     .map(|c| (c, 0_u32)).collect::<HashMap<_, _>>();
-
 fn gen_kmers(msa: &str, kmer_len: usize) -> Vec<usize> {
     let characters: Vec<&str> = vec!["A", "C", "G", "T"];
 
@@ -46,27 +39,12 @@ fn gen_kmers(msa: &str, kmer_len: usize) -> Vec<usize> {
         .collect::<HashMap<_, _>>();
 
     println!("combination length {}", combinations.len());
-    // println!("{:?}", combinations);
 
-    // let filename = "5343819_128Taxa_GTR+G__alpha2__100000_59873.phy";
-    //
-    // let contents: String = fs::read_to_string(filename)
-    //     .expect("Something went wrong reading the file");
-
-    // let s1 = vec!["acgtacgttg", "atttaaaaaaaaa", "acgttccttttt", "acgtccccccc"];
     let lines: Vec<&str> = msa
         .lines()
         .filter(|x| x.len() > 0)
         .map(|c| c.split(" ").last().unwrap())
         .collect();
-
-    // println!("number of taxa: {:?}", lines.len());
-    // println!("{:?}", lines.first().unwrap());
-
-    // let mut hmap: HashMap<&str, u32> = HashMap::new();
-    // for (c, x) in combinations.iter().enumerate(){
-    //     hmap.insert(x, c as u32);
-    // }
 
     let res: Vec<_> = lines
         .par_iter()
@@ -78,11 +56,9 @@ fn gen_kmers(msa: &str, kmer_len: usize) -> Vec<usize> {
             (c, m)
         })
         .collect();
-    // println!("Length of sequences: {:?}", res);
 
     let mut counts: Vec<_> = vec![0 as usize; combinations.len()];
 
-    // (0..res.len()).map(|x| res[x].1.iter().map(|&c | {counts[c] += 1; c})).collect::<Vec<usize>>();
     for x in res.iter() {
         for k in x.1.iter() {
             counts[*k] += 1;
